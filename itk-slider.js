04ffeb907a68887f8067195313c747f11331edf5
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-07-25 17:29:50 +0200

    add checkJQuery function

--- itk-slider.js
+++ itk-slider.js
@@ -1,5 +1,16 @@
 (function(){
 	var itkSlider = {
+		checkJQuery: function(){
+			if(!window.jQuery){
+				var script = document.createElement('script');
+				script.type = "text/javascript";
+				script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+				document.getElementsByTagName('head')[0].appendChild(script);
+				script.onload = this.buildSliders;
+				return false;
+			};
+			return true;
+		},
 		buildSliders: function(){
 			if(!this.checkJQuery()) return;
 			var sliders = document.getElementsByClassName("itk-slider");
