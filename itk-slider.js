f24c2b97de9cf1278c231f33a8f44bb77a0eae8f
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-07-25 17:27:00 +0200

    add buildSliders function

--- itk-slider.js
+++ itk-slider.js
@@ -1,5 +1,13 @@
 (function(){
 	var itkSlider = {
+		buildSliders: function(){
+			if(!this.checkJQuery()) return;
+			var sliders = document.getElementsByClassName("itk-slider");
+			var i;
+			for(i = 0; i < sliders.length; i++){
+				this.createSlider(sliders[i]);
+			};
+		},
 		init: function(){
 			if(window.addEventListener){
 				window.addEventListener("load", this.buildSliders);
