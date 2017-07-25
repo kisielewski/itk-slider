1ffc64cc7e34e039cb5a05b7cbbdf5a9a57faef8
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-07-25 17:20:17 +0200

    add init function

--- itk-slider.js
+++ itk-slider.js
@@ -1,5 +1,12 @@
 (function(){
 	var itkSlider = {
-		
+		init: function(){
+			if(window.addEventListener){
+				window.addEventListener("load", this.buildSliders);
+			} else if(window.attachEvent){
+				window.attachEvent("onload", this.buildSliders);
+			};
+		},
 	};
+	itkSlider.init();
 })();
